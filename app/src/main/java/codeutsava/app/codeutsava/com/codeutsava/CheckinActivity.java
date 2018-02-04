8a4a117c05e4bffcc0936ee93bdca3922e8f0d60
ayush <ayushjain17aug@gmail.com>
2018-02-04 10:38:58 +0530

    MapsDone

--- app/src/main/java/codeutsava/app/codeutsava/com/codeutsava/CheckinActivity.java
+++ app/src/main/java/codeutsava/app/codeutsava/com/codeutsava/CheckinActivity.java
@@ -1,13 +1,16 @@
 package codeutsava.app.codeutsava.com.codeutsava;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 
-public class CheckinActivity extends AppCompatActivity {
+public class CheckinActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    public Button enter;
+    public Button leave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +19,17 @@ public class CheckinActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        enter = (Button) findViewById(R.id.enter);
+        leave = (Button) findViewById(R.id.leave);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
